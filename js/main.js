b44f7dbdb28a177bf52a7eb26d6bac7cbc966f31
Student <student@ECAW01152-PC03.local>
2017-01-10 20:42:59 -0500

    First Commit, Notes 01

---
+++ js/main.js
@@ -0,0 +1,31 @@
+/* Document.write() is a Javascropt method that is used to display whatever your place indie of its round braces. USed document.write() to diplay Javascript content on HTML/PHP pages. */
+document.write('Hello, World!');
+
+/* Variables are used by programming languages to hold data fro repeated and later use. */
+var myFirstVariable = 25;
+document.write('<br>' + myFirstVariable);
+
+/*Javascript, like all programming languages, is capable of performing very complex and very simple math. BUt like all programminglanguages, you have to be very explicit about what you want the computer to do. Below, we will create two varaibles, add them together then display them on the screen. */
+var firstNumber = 16;
+var secondNumber = 4;
+var sumOfNumbers = firstNumber + secondNumber;
+document.write('<br>' + sumOfNumbers);
+
+/* Below, we will subract the above firstNumber and secondNumber from each other and display it on the screen. */
+var subOfNumbers = firstNumber - secondNumber;
+document.write('<br>' + subOfNumbers);
+
+/*Data types are different forms of data that are recognized by computers and operated on using programming languages. The most basic datatype is a bumber. */
+var numberDataType = 58;
+
+/* The second most basic datatype is a string. A string, in other words, is a letter, or a sentence, a paragraph, or anything that is not a true number that can be operated on by math operations. Strings are always placed inside of quotes. */
+var stringDataType = 'This is a string.';
+var alsoStringDataType = '565';
+var testNumber = firstNumber + alsoStringDataType;
+document.write('<br>' + testNumber);
+
+// This is a single line comment. It's meant for ver short notes.
+
+/* this is a multi line comment. It is mean't for longer comments that may be paragraphs long. */
+
+
